ae959c043518b2a86239696f7aaa119315c344e8
dvasilyev <dvasilyev@apple.com>
2019-10-28 21:05:28 -0700

    Adds params by Value and by Name

--- src/main/scala/com/study/scala/core/params/Params.scala
+++ src/main/scala/com/study/scala/core/params/Params.scala
@@ -1,10 +1,25 @@
 package com.study.scala.core.params
 
+import scala.util.Random
+
 object Params {
 
   def sumParams(first: Int, second: Int = 5): Int = {
     first + second
   }
+
+  def paramByName(value: => Int): Unit = {
+    println(value)
+    println(value)
+    println("----")
+  }
+
+  def paramByValue(value: Int): Unit = {
+    println(value)
+    println(value)
+    println("----")
+  }
+
 
   def main(args: Array[String]): Unit = {
 
@@ -15,5 +30,9 @@ object Params {
     // calling function by named params
     sumParams(second = 4, first = 1)
 
+    // calling function by name and by value
+    val random = new Random(100)
+    paramByValue(random.nextInt(98))
+    paramByName(random.nextInt(98))
   }
 }
