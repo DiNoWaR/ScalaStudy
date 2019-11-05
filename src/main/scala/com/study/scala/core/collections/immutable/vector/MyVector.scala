aebd414b996465d25a2ba89cf007353571e66ec7
dvasilyev <dvasilyev@apple.com>
2019-11-05 10:13:03 -0800

    Adds Vector

--- src/main/scala/com/study/scala/core/collections/immutable/vector/MyVector.scala
+++ src/main/scala/com/study/scala/core/collections/immutable/vector/MyVector.scala
@@ -7,10 +7,28 @@ package com.study.scala.core.collections.immutable.vector
   * It is backed by a little endian bit-mapped vector trie with a branching factor of 32.
   * Locality is very good, but not contiguous, which is good for very large sequences.
   */
-class MyVector {
+object MyVector {
 
   def main(args: Array[String]): Unit = {
 
+    val names = Vector("Denis", "Max", "James") ++ Set("","")
+
+    //prepending element creating new Vector
+    val namesWithKevin = "Kevin" +: names
+
+    //prepending multiple elements creating new Vector
+    val namesWithPrependedList = Seq("Greg", "Tom") ++: names
+
+    //appending element creating new Vector
+    val namesWithSarah = names :+ "Sarah"
+
+    //appending multiple elements creating new Vector
+    val namesWithAppendedList = names ++ Seq("Kirk", "Lars")
+
+    //concatenating 2 vector creating new vector
+    val otherNames = Vector("Jack", "Black")
+
+    val combinedNames = names ++ otherNames
 
   }
 }
